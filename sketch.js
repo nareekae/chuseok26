0c8f9c9e14a7ca11cd1ae3d0f9b07e48d9bc4410
nareekae <nareenkae@gmail.com>
2026-09-20 20:13:26 -0700

    Adjust sprite scale range in configuration

--- sketch.js
+++ sketch.js
@@ -30,8 +30,8 @@ let leftHandPusher, rightHandPusher, nosePusher;
 
 const CFG = {
   spriteCount: 40,
-  spriteScaleMin: 0.5,
-  spriteScaleMax: 0.9,
+  spriteScaleMin: 0.1,
+  spriteScaleMax: 0.5,
   airFriction: 0.05,
   restitution: 0.85,
   gravityY: 0.001,
